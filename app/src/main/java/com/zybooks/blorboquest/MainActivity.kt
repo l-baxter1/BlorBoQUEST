50571a209381a4dfaf55fe616783c27503a55110
l-baxter1 <l-baxter@onu.edu>
2024-04-19 00:47:40 -0400

    "click here for money" button base functionality "upgrade" button base functionality

--- app/src/main/java/com/zybooks/blorboquest/MainActivity.kt
+++ app/src/main/java/com/zybooks/blorboquest/MainActivity.kt
@@ -2,10 +2,37 @@ package com.zybooks.blorboquest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var moneyButton: Button
+    private lateinit var cashTextBox: TextView
+
+    private var totalCash = 0
+    private var cashPerClick = 1
+    private var clickMultiplier = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moneyButton = findViewById(R.id.moneyButton)
+        cashTextBox = findViewById(R.id.cashNumber)
+
+        cashTextBox.text = totalCash.toString()
+    }
+
+    fun onMoneyButtonClick(view: View) {
+        totalCash += (cashPerClick * clickMultiplier)
+        cashTextBox.text = totalCash.toString()
+    }
+
+    fun onUpgradeButtonClick(view: View) {
+        clickMultiplier *= 2
     }
 }
