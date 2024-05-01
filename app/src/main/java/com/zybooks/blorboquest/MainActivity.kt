d30ba7f65b245c4ed5a63dd1b5c3a4248b09f3c0
l-baxter1 <l-baxter@onu.edu>
2024-05-01 15:10:16 -0400

    colors n stuff

--- app/src/main/java/com/zybooks/blorboquest/MainActivity.kt
+++ app/src/main/java/com/zybooks/blorboquest/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.zybooks.blorboquest
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -18,17 +21,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cashBox: TextView
     private lateinit var background: ConstraintLayout
     private lateinit var multiplierBox: TextView
-    private lateinit var downgradeButton: Button
-    private lateinit var upgradeButton: Button
+    private lateinit var downgradeButton: ImageButton
+    private lateinit var upgradeButton: ImageButton
     private lateinit var downgradeCostBox: TextView
     private lateinit var upgradeCostBox: TextView
 
-    private var totalCash = 0
-    private var cashPerClick = 1
-    private var clickMultiplier = 1
-    private var blorboMultiplier = 1
-    private var downgradeCost = 1
-    private var upgradeCost = 1
+    private var totalCash = 0.0
+    private var cashPerClick = 1.0
+    private var clickMultiplier = 1.0
+    private var blorboMultiplier = 1000000.0
+    private var downgradeCost = 1.0
+    private var upgradeCost = 1.0
     private var abbr = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,15 +43,26 @@ class MainActivity : AppCompatActivity() {
         moneyButton = findViewById(R.id.moneyButton)
         cashBox = findViewById(R.id.cashBox)
         background = findViewById(R.id.main)
+        upgradeCostBox = findViewById(R.id.upgradeCostBox)
+        downgradeCostBox = findViewById(R.id.downgradeCostBox)
+        upgradeButton = findViewById(R.id.upgradeButton)
+        downgradeButton = findViewById(R.id.downgradeButton)
+        multiplierBox = findViewById(R.id.multiplierBox)
 
         background.background = getDrawable(R.drawable.placeholder_bg)
 
         setMoneyBox(cashBox, totalCash, abbr)
         setMoneyBox(upgradeCostBox, upgradeCost, abbr)
         setMoneyBox(downgradeCostBox, downgradeCost, abbr)
-        setMultBox(cashBox, totalCash)
+        setMultBox(multiplierBox, clickMultiplier)
 
     }
+//    val mainHandler = Handler(Looper.getMainLooper())
+//    mainHandler.post(object : Runnable {
+//        override fun run() {
+//            mainHandler.postDelayed(this, 1000)
+//        }
+//    })
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.nav_menu, menu)
         return super.onCreateOptionsMenu(menu)
@@ -64,20 +78,56 @@ class MainActivity : AppCompatActivity() {
     fun onMoneyButtonClick(view: View) {
         totalCash += (cashPerClick * clickMultiplier)
         setMoneyBox(cashBox, totalCash, abbr)
+        if (totalCash >= upgradeCost) {
+            upgradeCostBox.setTextColor(Color.parseColor("#ffffff"))
+        } else {
+            upgradeCostBox.setTextColor(Color.parseColor("#ff0000"))
+        }
+        if (totalCash >= downgradeCost) {
+            downgradeCostBox.setTextColor(Color.parseColor("#ffffff"))
+        } else {
+            downgradeCostBox.setTextColor(Color.parseColor("#ff0000"))
+        }
     }
     fun onUpgradeButtonClick(view: View) {
         if (totalCash >= upgradeCost) {
             totalCash -= upgradeCost
-            clickMultiplier *= 2
+            clickMultiplier *= 1.25
             upgradeCost *= 4
             setMultBox(multiplierBox, clickMultiplier)
-            setMoneyBox(upgradeCostBox, totalCash, abbr)
+            setMoneyBox(upgradeCostBox, upgradeCost, abbr)
+            setMoneyBox(cashBox, totalCash, abbr)
+
+            //update text colors
+            if (totalCash < upgradeCost) {
+                upgradeCostBox.setTextColor(Color.parseColor("#ff0000"))
+            }
+            if (totalCash < downgradeCost) {
+                downgradeCostBox.setTextColor(Color.parseColor("#ff0000"))
+            }
         }
     }
-    fun setMoneyBox(box: TextView, value: Int, abbr: String) {
+    fun onDowngradeButtonClick(view: View) {
+        if (totalCash >= upgradeCost) {
+            totalCash -= upgradeCost
+            blorboMultiplier /= 1.25
+            downgradeCost *= 4
+            setMoneyBox(downgradeCostBox, downgradeCost, abbr)
+            setMoneyBox(cashBox, totalCash, abbr)
+
+            //update text colors
+            if (totalCash < upgradeCost) {
+                upgradeCostBox.setTextColor(Color.parseColor("#ff0000"))
+            }
+            if (totalCash < downgradeCost) {
+                downgradeCostBox.setTextColor(Color.parseColor("#ff0000"))
+            }
+        }
+    }
+    fun setMoneyBox(box: TextView, value: Double, abbr: String) {
         box.text = "$" + value + abbr
     }
-    fun setMultBox(box: TextView, value: Int) {
-        //box.text = (value + "x").toString()
+    fun setMultBox(box: TextView, value: Double) {
+        box.text = "Multiplier: x" + value
     }
 }
