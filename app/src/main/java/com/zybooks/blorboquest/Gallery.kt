d316fd549e8907c3bd87a15e5b7288b6d0825c5a
DMac-MacDonald <112093094+DMac-MacDonald@users.noreply.github.com>
2024-05-03 14:54:03 -0400

    gallery done

--- app/src/main/java/com/zybooks/blorboquest/Gallery.kt
+++ app/src/main/java/com/zybooks/blorboquest/Gallery.kt
@@ -4,12 +4,37 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class Gallery: AppCompatActivity() {
+    private lateinit var rightButton: Button
+    private lateinit var leftButton: Button
+    private lateinit var galleryImage:ImageView
+    var galIndex = 0
+    val galleryImages = arrayOf(
+        R.drawable.gallery1 ,
+        R.drawable.gallery2,
+        R.drawable.gallery3,
+        R.drawable.gallery4,
+        R.drawable.gallery5,
+        R.drawable.gallery6,
+        R.drawable.gallery7,
+        R.drawable.gallery8,
+        R.drawable.gallery9,
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
+
+        rightButton = findViewById(R.id.right_button)
+        leftButton = findViewById(R.id.left_button)
+        galleryImage = findViewById(R.id.galleryImageView)
+
+
         supportActionBar?.setIcon(R.drawable.menu_icon)
         setSupportActionBar(findViewById(R.id.nav_menu))
     }
@@ -25,5 +50,20 @@ class Gallery: AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
 
+    }
+    fun onRightClicked(view: View) {
+        galIndex++
+        if (galIndex > 8){
+            galIndex = 0
+        }
+        galleryImage.setImageResource(galleryImages[galIndex])
+
+    }
+    fun onLeftClicked(view: View) {
+        galIndex--
+        if(galIndex == 0){
+            galIndex = 8
+        }
+        galleryImage.setImageResource(galleryImages[galIndex])
     }
 }
--- app/src/main/java/com/zybooks/blorboquest/Settings.kt
+++ app/src/main/java/com/zybooks/blorboquest/Settings.kt
@@ -17,13 +17,14 @@ class Settings: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         supportActionBar?.setIcon(R.drawable.menu_icon)
         setSupportActionBar(findViewById(R.id.nav_menu))
         val spinner: Spinner = findViewById(R.id.backgrounds_spinner)
         val defaultTextForBackgroundSpinner = "Background Color"
         val backgroundColors: MutableList<String?> = ArrayList()
         backgroundColors.add(0, "Select Background Color")
-        backgroundColors.add("Black")
+        backgroundColors.add("Brown")
         backgroundColors.add("Yellow")
         backgroundColors.add("Green")
         backgroundColors.add("Blue")
@@ -31,6 +32,7 @@ class Settings: AppCompatActivity() {
         backgroundColors.add("Pink")
 
         val settings_layout = findViewById<ConstraintLayout>(R.id.settingsLayout)
+        //R.color.default_back_color=663106
         val arrayAdapter: ArrayAdapter<String?> = ArrayAdapter<String?>(this, android.R.layout.simple_list_item_1, backgroundColors)
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = arrayAdapter
@@ -41,7 +43,22 @@ class Settings: AppCompatActivity() {
                     settings_layout.setBackgroundResource(R.color.default_back_color)
                 }
                 if(parent.getItemAtPosition(position)== "Yellow"){
-                    settings_layout.setBackgroundResource(R.color.yellow_back_color)
+                   // settings_layout.setBackgroundResource(R.color.yellow_back_color)
+                }
+                if(parent.getItemAtPosition(position)== "Brown"){
+                    settings_layout.setBackgroundResource(R.color.brown_back_color)
+                }
+                if(parent.getItemAtPosition(position)== "Green"){
+                    settings_layout.setBackgroundResource(R.color.green_back_color)
+                }
+                if(parent.getItemAtPosition(position)== "Blue"){
+                    settings_layout.setBackgroundResource(R.color.blue_back_color)
+                }
+                if(parent.getItemAtPosition(position)== "Purple"){
+                    settings_layout.setBackgroundResource(R.color.purple_back_color)
+                }
+                if(parent.getItemAtPosition(position)== "Pink"){
+                    settings_layout.setBackgroundResource(R.color.pink_back_color)
                 }
                 else {
                     val item = parent.getItemAtPosition(position).toString()
