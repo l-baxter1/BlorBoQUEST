b2da8b535de327d37ad8f8b3b49724313373c37e
Destiny Morrison <d-morrison.2@onu.edu>
2024-05-14 21:17:01 -0400

    untracked

---
+++ app/src/main/java/com/zybooks/blorboquest/email_fragment.kt
@@ -0,0 +1,27 @@
+package com.zybooks.blorboquest
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class Email : Fragment() {
+
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_email_fragment, container, false)
+
+    }
+
+
+
+
+
+}
